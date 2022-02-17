2aa08d8e99f29f691df1703d6148be2ffe91df2a
Niktesla02 <niktesla02@gmail.com>
2022-02-17 18:54:22 +0100

    new changes

---
+++ circle.py
@@ -0,0 +1,5 @@
+import math as m
+
+radius=float(input("Radius"))
+area = m.pi * radius*radius
+print(area)
---
+++ two_times_two.py
@@ -0,0 +1,10 @@
+#Takes a value in a list, multiplies the value by 2, and adds the product to a variable total
+numbers = [2,4,6,8]
+
+def times_two(values):
+    total=0
+    for num in numbers:
+        total += num*2
+    return total
+
+print(times_two(numbers))
